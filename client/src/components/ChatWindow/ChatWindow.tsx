57c4a00ea56d814cec81a366535a3e309bc436a4
AjshaC <94913424+AjshaC@users.noreply.github.com>
2023-08-16 15:40:06 +0200

    LayoutOfChatPage-Done

--- client/src/components/ChatWindow/ChatWindow.tsx
+++ client/src/components/ChatWindow/ChatWindow.tsx
@@ -1,13 +1,26 @@
 import "./ChatWindow.css"
+import { Alert } from 'antd';
+
+
 
 export default function ChatWindow(){
     return ( 
-        <div>
-            <h1>UserName Joined Chat</h1>
-            <p>UserName</p>
-            <p>message</p>
-            <p>time</p>
+       
+        <div className="ChatWindow">
+            <div className="UserJoined">
+            <h4>UserName Joined Chat</h4>
+                
+            </div>
+            
+            <div className="Userinfo">
+             <p>UserName</p>
+             <p>12:50</p>
+            </div>
+        
+            <Alert className="message" message="Success Text" type="success" />
 
         </div>
     )
 }
+
+
