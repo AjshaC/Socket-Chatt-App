78cf8749e7958bf0cb1235d1b6bd8aa3d0983cf7
AjshaC <94913424+AjshaC@users.noreply.github.com>
2023-08-18 10:25:20 +0200

    .

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const io = new Server(server, {
 
 //send message
     socket.on('send_message', (message) => {
-      socket.emit('receive_message', message) //KOMPLETTERA MED ROOM-ID HÄR SEDAN!!!!
+      socket.broadcast.emit('receive_message', message) //KOMPLETTERA MED ROOM-ID HÄR SEDAN!!!!
       console.log(message);
     })
 //send message
