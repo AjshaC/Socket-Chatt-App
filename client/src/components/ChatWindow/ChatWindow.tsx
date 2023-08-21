159a1516eee72a5a9bf7c7d7893d93f8c37b7e35
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-08-21 12:52:35 +0200

    css updates

--- client/src/components/ChatWindow/ChatWindow.tsx
+++ client/src/components/ChatWindow/ChatWindow.tsx
@@ -7,6 +7,7 @@ import { SendOutlined } from "@ant-design/icons";
 import ScrollToBottom from "react-scroll-to-bottom";
 
 export default function ChatWindow() {
+
   const {
     userJoined,
     socket,
@@ -18,7 +19,18 @@ export default function ChatWindow() {
   } = useChatContext();
 
   const [showAlert, setShowAlert] = useState(false);
+  //const [typingUsers, setTypingUsers] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
 
+  const startTyping = () => {
+      setIsTyping(true);
+    };
+  
+  const stopTyping = () => {
+      setIsTyping(false);
+  };
+
+  //SHOW THAT A NEW USER JOIN CHAT
   useEffect(() => {
     if (userJoined) {
       setShowAlert(true);
@@ -29,60 +41,15 @@ export default function ChatWindow() {
     }
   }, [userJoined]);
 
+
   //SEND MESSAGE
-  const sendMessage = async () => {
-    if (currentMessage !== "") {
-      const now = new Date();
-      const hours = now.getHours();
-      const minutes = now.getMinutes();
-
-      const formattedMinutes = (minutes < 10 ? "0" : "") + minutes;
-
-export default function ChatWindow(){
-    const {
-        userJoined, 
-        socket, 
-        user, 
-        currentMessage, 
-        setCurrentMessage, 
-        messageList, 
-        setMessageList, 
-        //typingUsers, 
-        //setTypingUsers
-    } = useChatContext();
- 
-    const [showAlert, setShowAlert] = useState(false);
-    //const [typingUsers, setTypingUsers] = useState([]);
-    const [isTyping, setIsTyping] = useState(false);
-
-    const startTyping = () => {
-        setIsTyping(true);
-      };
-    
-    const stopTyping = () => {
-        setIsTyping(false);
-    };
-
-
-    //SHOW THAT A NEW USER JOIN CHAT
-    useEffect(() => {
-        if (userJoined) {
-            setShowAlert(true);
-            const timeout = setTimeout(() => {
-                setShowAlert(false);
-            }, 5000); // Hide after 10 seconds
-            return () => clearTimeout(timeout);
-        }
-    }, [userJoined]);
-
-    //SEND MESSAGE
     const sendMessage = async () => {
 
-        if (currentMessage !== "") {
-            const now = new Date();
-            const hours = now.getHours();
-            const minutes = now.getMinutes();
-            const formattedMinutes = (minutes < 10 ? "0" : "") + minutes;
+      if (currentMessage !== "") {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        const formattedMinutes = (minutes < 10 ? "0" : "") + minutes;
 
       const messageData = {
         //room: room, //KOMPLETTERA MED DETTA SENARE!!!
@@ -93,28 +60,22 @@ export default function ChatWindow(){
 
       await socket.emit("send_message", messageData);
       setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
     }
   };
-            await socket.emit("send_message", messageData);
-            setMessageList((list) => [...list, messageData]);
-            setCurrentMessage("");
-        }
-    };
+           
 
   useEffect(() => {
     socket.on("receive_message", (message) => {
       setMessageList((list) => [...list, message]);
     });
   }, [socket]);
-    useEffect(() => {
-        socket.on("receive_message", (message) => {
-            setMessageList((list) => [...list, message]);
-        });
-    }, [socket]);
 
-
+ 
   return (
-    <div className="ChatWindow">
+
+  <div className="ChatWindow">
+
       <div className="UserJoined">
         {showAlert && (
           <Alert
@@ -127,91 +88,43 @@ export default function ChatWindow(){
       </div>
 
       <div className="MessageContainer">
-        {messageList.map((messageContent) => {
-          return (
-            <div className="MessageBox">
-              <div className="MessageContent">
-                <p>{messageContent.message}</p>
-              </div>
-              <div className="MessageMeta">
-                <p>{messageContent.time}</p>
-                <p>{messageContent.author}</p>
-              </div>
-            </div>
-          );
-        })}
-    return ( 
-       
-        <div className="ChatWindow">
-            
-            <div className="UserJoined">
-            {showAlert && (
-                    <Alert
-                        message={` ${userJoined} Joined Chat`}
-                        type="success"
-                        showIcon
-                        closable
-                    />
-                )}
-            </div>
+        <ScrollToBottom>
+          {messageList.map((messageContent) => {
+            return (
+              <div className="MessageBox"> 
 
-            <div className="MessageContainer">
-                <ScrollToBottom>
-
-                {/* PLACERAR DENNA DÄR DET PASSAR NÄR CSS ÄR KLAR FÖR CHATTFÖNSTRET */}
-                <div>
-                
+                <div className="MessageContent">
+                  <p>{messageContent.message}</p>
                 </div>
 
-                {messageList.map((messageContent) => {
-                    return (
-                        <div className="MessageBox"> 
-                            <div className="MessageContent">
-                                <p>{messageContent.message}</p>
-                            </div>
-                            <div className="MessageMeta">
-                                <p>{messageContent.time}</p>
-                                <p>{messageContent.author}</p>
-                            </div>
-                        </div>
-                    )
-                })}
-
-        <Alert className="message2" message="Success Text" type="success" />
-      </div>
-                {/*This is triggered when a user is typing*/}
-                <div className="IsTyping">
-                    {isTyping && <p>{user} is typing...</p>}
+                <div className="MessageMeta">
+                  <p>{messageContent.time}</p>
+                  <p>{messageContent.author}</p>
                 </div>
-                </ScrollToBottom>
-            </div>
+              </div>
+            )
+          })}
 
-      <div className="ChatFooter">
-        <Input
-          className="SendInput"
-          onChange={(e) => setCurrentMessage(e.target.value)}
-          type="text"
-          placeholder="Write your message..."
-        />
-        <Button className="SendBtn" onClick={sendMessage} type="primary">
-          <SendOutlined />
-        </Button>
-      </div>
+        {/*This is triggered when a user is typing*/}
+          <div className="IsTyping">
+            {isTyping && <p>{user} is typing...</p>}
+          </div>
+        
+          </ScrollToBottom>
+
+          <div className="ChatFooter">
+            <Input
+              className="SendInput" 
+              onChange={(e)=> setCurrentMessage(e.target.value)} 
+              onKeyDown= {(e) => {e.key === "Enter" && sendMessage();}}
+              onFocus={startTyping}
+              onBlur={stopTyping}
+              type="text" 
+              value={currentMessage} 
+              placeholder="Write your message..." />
+
+            <Button className="SendBtn" onClick={sendMessage} type="primary"><SendOutlined /></Button>
+          </div>
     </div>
-  );
-            <div className="ChatFooter">
-                <Input 
-                    onChange={(e)=> setCurrentMessage(e.target.value)} 
-                    onKeyDown= {(e) => {e.key === "Enter" && sendMessage();}}
-                    onFocus={startTyping}
-                    onBlur={stopTyping}
-                    type="text" 
-                    value={currentMessage} 
-                    placeholder="Write your message..." />
-
-                <Button onClick={sendMessage} type="primary"><SendOutlined /></Button>
-            </div>
-
-        </div>
-    )
-}
+  </div>
+)}
