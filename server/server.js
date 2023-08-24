2373e9490578baee4c4297c3b98f6783e8f9ba1a
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-08-24 11:10:02 +0200

    basic setup for roomlist

--- server/server.js
+++ server/server.js
@@ -13,11 +13,6 @@ const io = new Server(server, {
   },
 });
 
-//const rooms = []; SÄTTA LOGIKEN FÖR ATT SPARA RUM? Listan som uppdateras
-const availableRooms = [];
-const rooms = io.sockets.adapter.rooms;
-const roomArray = Array.from(rooms.keys());
-
 
 io.on("connection", (socket) => {
   //CONNECT TO SERVER
@@ -27,17 +22,27 @@ io.on("connection", (socket) => {
   const user = socket.handshake.auth.user;
   console.log(user);
 
-
   //ROOM
   socket.on("join_room", (room) => {
     socket.join(room);
     socket.broadcast.emit("userJoined", user);
-    console.log(io.sockets.adapter.rooms);
-    console.log("test");
-    console.log(roomArray);
-    socket.emit("room_array", roomArray);
-    socket.broadcast.emit("userJoined", user); //ADD ROOM
-    console.log(`User with ID: ${socket.id} and username ${user}, joined room: ${room}`);
+    // console.log(`User with ID: ${socket.id} and username ${user}, joined room: ${room}`);
+
+    const availableRooms = []
+    const rooms = io.sockets.adapter.rooms;
+    console.log("ROOMS", rooms);
+
+    //Loop over the Map items where key and value are not the same
+    for (const [key, value] of rooms) {
+      if (key !== value && !(value.size === 1 && value.has(key))) {
+
+       //push to our room array
+      availableRooms.push(key);
+      console.log("AVAILABLE ROOMS", availableRooms);
+  }
+}
+
+    socket.emit("room_array", availableRooms);
   });
 
 
