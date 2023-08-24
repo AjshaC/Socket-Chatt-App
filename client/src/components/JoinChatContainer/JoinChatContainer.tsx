30ba1e99c0977a983c875860f98b27d6cef45cbe
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-08-24 22:12:27 +0200

    show errormessage if no username is choose before join chat

--- client/src/components/JoinChatContainer/JoinChatContainer.tsx
+++ client/src/components/JoinChatContainer/JoinChatContainer.tsx
@@ -2,30 +2,27 @@ import "./JoinChatContainer.css";
 import { useNavigate } from "react-router-dom";
 import { Button, Input, Card, Space } from "antd";
 import { useChatContext } from "../../context/chatContext";
+import { useState } from "react";
 
 
 export default function JoinChatContainer() {
-  const { setUser, connectToTheServer } = useChatContext();
+  const { user, setUser, connectToTheServer } = useChatContext();
+  const [errorInfo, setErrorInfo] = useState("");
 
   const navigate = useNavigate();
 
-  /*const joinChatClick = () => {
+  const joinChatClick = () => {
         if(user.trim() !== ""){
             connectToTheServer()
             navigate("/chat");
         }
-                   
+         
         else{
-             console.log("no Username", user)
+          setErrorInfo("*Username is required")
         }
-        
-    }*/
+    }
 
-  const joinChatClick = () => {
-    connectToTheServer();
-    navigate("/chat");
-  };
-
+ 
   return (
     <>
       <Card
@@ -40,6 +37,7 @@ export default function JoinChatContainer() {
           alignItems: "center",
         }}
       >
+        
         <Space.Compact className="InputAndButton" style={{ width: "100%" }}>
           <Input
             placeholder="Enter username ..."
@@ -51,10 +49,14 @@ export default function JoinChatContainer() {
               border: "none",
             }}
           />
+
           <Button type="primary" onClick={joinChatClick}>
             Join
           </Button>
         </Space.Compact>
+
+        <p className="errorInfo">{errorInfo}</p>
+
       </Card>
     </>
   );
