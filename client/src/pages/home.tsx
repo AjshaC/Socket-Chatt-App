16fb395bfff99a0b5a7f9dbfd44a72f510cd4af2
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-08-27 16:55:28 +0200

    add footer

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,10 +1,12 @@
 import JoinChatContainer from "../components/JoinChatContainer/JoinChatContainer";
+import Footer from "../components/Footer/Footer";
 import "./Home.css"
 export default function Home(){
     return(
         <div className="HomeWrapper">
             <h1 className="NeonText"><span className="LogoUppercase">chat</span> at eleven</h1>
             <JoinChatContainer/>
+            <Footer></Footer>
         </div>
     )
     
