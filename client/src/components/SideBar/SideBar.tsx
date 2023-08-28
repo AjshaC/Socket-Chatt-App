f195ec327e6a0aba8912753aa06427497ff35a45
Viola Sorgato <egles.sorgato@medieinstitutet.se>
2023-08-29 00:32:54 +0200

    Can display only own name

--- client/src/components/SideBar/SideBar.tsx
+++ client/src/components/SideBar/SideBar.tsx
@@ -1,35 +1,50 @@
 import "./SideBar.css";
 import { useState, useEffect } from "react";
 import { useChatContext } from "../../context/chatContext";
-import { Button} from "antd";
-import { UserOutlined, ArrowRightOutlined, TeamOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import {
+  UserOutlined,
+  ArrowRightOutlined,
+  TeamOutlined,
+} from "@ant-design/icons";
 
 interface RoomData {
   roomName: string;
-  users: string[];
+  usernames: string[];
 }
-
 
 export default function SideBar() {
   const { socket, user, room, setRoom } = useChatContext();
   const [roomArray, setRoomArray] = useState([]);
   const [usersInRoom, setUsersInRoom] = useState<RoomData[]>([]);
 
-
   useEffect(() => {
-  socket.emit("get_room_array"); // Request roomArray from server
+    socket.emit("get_room_array"); // Request roomArray from server
 
-  socket.on("room_array", (data) => {
-  setRoomArray(data); // Update the state with roomArray from the server
-  });
+    socket.on("room_array", (data) => {
+      setRoomArray(data); // Update the state with roomArray from the server
+    });
 
-  socket.on("users_in_room", (data) => {
-    console.log(data); // Update the state with roomArray from the server
-    setUsersInRoom(data);
+    // socket.on("users_in_room", (data) => {
+    //   console.log(data); // Update the state with roomArray from the server
+    //   setUsersInRoom(data);
+    // });
+
+    socket.on("users_in_room", (data) => {
+      const usersWithUsernames = data.map(
+        (roomData: { roomName: string; users: string[] }) => ({
+          roomName: roomData.roomName,
+          usernames: roomData.users.map((userId: string) =>
+            userId === socket.id ? user : userId
+          ),
+        })
+      );
+      setUsersInRoom(usersWithUsernames);
+      console.log(usersWithUsernames);
     });
   }, []);
 
-  const joinRoom = (newRoom : string) => {
+  const joinRoom = (newRoom: string) => {
     if (room) {
       socket.emit("leave_room", room); // Leave the old room
     }
@@ -37,13 +52,15 @@ export default function SideBar() {
     setRoom(newRoom);
   };
 
-
   return (
     <div className="SideBar">
-
       <div className="ChatInLog">
-        <p className="UserInfo"><UserOutlined /> {user}</p>
-        <p className="RoomInfo"><ArrowRightOutlined /> You are in Room - {room}</p>
+        <p className="UserInfo">
+          <UserOutlined /> {user}
+        </p>
+        <p className="RoomInfo">
+          <ArrowRightOutlined /> You are in Room - {room}
+        </p>
       </div>
 
       {/* <ul>
@@ -58,27 +75,62 @@ export default function SideBar() {
         ))}
       </ul> */}
 
+      {/* <div className="RoomList">
+        {roomArray.map((room, index) => (
+          <div key={index}>
+            <Button
+              type="primary"
+              key={index}
+              className="RoomButton"
+              onClick={() => joinRoom(room)}
+            >
+              {room}
+            </Button>
+            <br />
+            <p className="UserHeader">
+              <TeamOutlined /> Users in the room
+            </p>
+            <ul>
+              {(
+                usersInRoom.find((data) => data.roomName === room)?.users || []
+              ).map((user) => (
+                <li key={user} className="UserInRoom">
+                  {user}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div> */}
+
       <div className="RoomList">
         {roomArray.map((room, index) => (
           <div key={index}>
             <Button
-              type="primary" 
-              key={index} 
+              type="primary"
+              key={index}
               className="RoomButton"
-              onClick={() => joinRoom(room)}>
-                {room}
+              onClick={() => joinRoom(room)}
+            >
+              {room}
             </Button>
             <br />
-            <p className="UserHeader"><TeamOutlined /> Users in the room</p>
+            <p className="UserHeader">
+              <TeamOutlined /> Users in the room
+            </p>
             <ul>
-              {(usersInRoom.find((data) => data.roomName === room)?.users || []).map((user) => (
-                <li key={user} className="UserInRoom">{user}</li>
+              {(
+                usersInRoom.find((data) => data.roomName === room)?.usernames ||
+                []
+              ).map((username) => (
+                <li key={username} className="UserInRoom">
+                  {username}
+                </li>
               ))}
             </ul>
           </div>
         ))}
       </div>
     </div>
-
   );
 }
